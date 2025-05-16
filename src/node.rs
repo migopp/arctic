3fa9d4415b023001fc669ec4e8e5e90d3502d271
Newton Ni <nwtnni@gmail.com>
2025-05-16 15:02:51 -0500

    Implement pessimistic traversal

--- src/node.rs
+++ src/node.rs
@@ -1,11 +1,13 @@
+use ribbit::atomic::A128;
 use ribbit::u48;
+use ribbit::unpack;
 
 mod node256;
 
 pub(crate) use node256::Node256;
 
 pub(crate) trait Node {
-    fn get(&self, key: &[u8]) -> Option<Match>;
+    fn get(&self, key: &[u8]) -> Option<&A128<Slot>>;
 }
 
 #[ribbit::pack(size = 128)]
@@ -14,8 +16,8 @@ pub(crate) struct Slot {
     len: u8,
 
     freeze: bool,
-    value: bool,
-    #[ribbit(size = 2)]
+
+    #[ribbit(size = 3)]
     kind: Kind,
     grow: bool,
 
@@ -23,20 +25,65 @@ pub(crate) struct Slot {
     next: u48,
 }
 
-#[ribbit::pack(size = 2)]
-pub(crate) enum Kind {
-    N4,
-    N256,
+impl Slot {
+    pub(crate) fn traverse(&self, key: &mut &[u8]) -> Traverse {
+        let search_len = key.len();
+        let search_key = *key;
+
+        let slot_len = self.len() as usize;
+        let slot_key = self.key().to_be_bytes();
+
+        assert!(slot_len <= 8);
+
+        let match_len = slot_key
+            .iter()
+            .take(slot_len as usize)
+            .zip(search_key)
+            .take_while(|(slot_byte, search_byte)| slot_byte == search_byte)
+            .count();
+
+        *key = &key[match_len..];
+
+        // Fast path: successful traversal
+        if search_len >= slot_len && slot_len == match_len {
+            return Traverse::Child(self.child());
+        }
+
+        assert!(
+            search_len >= slot_len || slot_len != match_len,
+            "Precondition: no key is a prefix of another key",
+        );
+
+        // Split out matching prefix
+        let mut split = slot_key;
+        split[match_len..].fill(0);
+        Traverse::Split(u64::from_be_bytes(split))
+    }
+
+    pub(crate) fn child(&self) -> Option<Ref> {
+        let pointer = self.next().value();
+
+        match self.kind().unpack() {
+            <unpack![Kind]>::Null => None,
+            <unpack![Kind]>::Value => Some(Ref::Value(pointer as *mut u64 as *mut ())),
+            <unpack![Kind]>::Node256 => Some(Ref::Node256(unsafe { &*(pointer as *mut Node256) })),
+        }
+    }
 }
 
-pub(crate) enum Match<'a> {
-    Full {
-        slot: &'a Slot,
-    },
+pub(crate) enum Ref<'a> {
+    Value(*mut ()),
+    Node256(&'a Node256),
+}
 
-    Partial {
-        slot: &'a Slot,
-        prefix: u8,
-        suffix: u8,
-    },
+#[ribbit::pack(size = 3)]
+pub(crate) enum Kind {
+    Null,
+    Value,
+    Node256,
+}
+
+pub(crate) enum Traverse<'a> {
+    Child(Option<Ref<'a>>),
+    Split(u64),
 }
