1a53e618e17cfe91961918942d852c22cd359c11
Newton Ni <nwtnni@gmail.com>
2025-09-26 16:04:19 -0500

    Add skeleton code for sequential operations

--- src/raw/sequential.rs
+++ src/raw/sequential.rs
@@ -14,6 +14,30 @@ impl Map {
         &self.root
     }
 
+    #[expect(dead_code, unused_variables)]
+    #[inline]
+    pub(crate) fn get<K: byte::Iterator>(&mut self, key: K) -> Option<u64> {
+        todo!()
+    }
+
+    #[expect(dead_code, unused_variables)]
+    #[inline]
+    pub(crate) fn insert<K: byte::Iterator>(&mut self, key: K, value: u64) -> Option<u64> {
+        todo!()
+    }
+
+    #[expect(dead_code, unused_variables)]
+    #[inline]
+    pub(crate) fn remove<K: byte::Iterator>(&mut self, key: K) -> Option<u64> {
+        todo!()
+    }
+
+    #[expect(dead_code, unused_variables)]
+    #[inline]
+    pub(crate) fn update<K: byte::Iterator>(&mut self, key: K, value: u64) -> Option<u64> {
+        todo!()
+    }
+
     pub(crate) fn preorder<K: byte::Stack, S: iter::Selector>(&mut self) -> iter::EntryIter<K, S> {
         iter::EntryIter::new(&mut self.root)
     }
