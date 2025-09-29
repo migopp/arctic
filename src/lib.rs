dcb02b12c40831875a5c60b0de0d612a617285d9
Newton Ni <nwtnni@gmail.com>
2025-09-29 12:51:09 -0500

    Move value trait into module

--- src/lib.rs
+++ src/lib.rs
@@ -23,37 +23,12 @@ mod raw;
 pub mod sequential;
 mod smr;
 pub mod stat;
+mod value;
 
 pub(crate) use edge::Edge;
 pub use key::Key;
 pub(crate) use node::Node;
-
-pub trait Value {
-    fn from_u64(value: u64) -> Self;
-    fn into_u64(self) -> u64;
-}
-
-impl Value for u32 {
-    #[inline]
-    fn from_u64(value: u64) -> Self {
-        value as u32
-    }
-
-    #[inline]
-    fn into_u64(self) -> u64 {
-        self as u64
-    }
-}
-
-impl Value for () {
-    #[inline]
-    fn from_u64(_: u64) -> Self {}
-
-    #[inline]
-    fn into_u64(self) -> u64 {
-        0
-    }
-}
+pub use value::Value;
 
 #[derive(Debug)]
 pub(crate) enum Or<L, R> {
---
+++ src/value.rs
@@ -0,0 +1,26 @@
+pub trait Value {
+    fn from_u64(value: u64) -> Self;
+    fn into_u64(self) -> u64;
+}
+
+impl Value for u32 {
+    #[inline]
+    fn from_u64(value: u64) -> Self {
+        value as u32
+    }
+
+    #[inline]
+    fn into_u64(self) -> u64 {
+        self as u64
+    }
+}
+
+impl Value for () {
+    #[inline]
+    fn from_u64(_: u64) -> Self {}
+
+    #[inline]
+    fn into_u64(self) -> u64 {
+        0
+    }
+}
