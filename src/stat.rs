edd6a195996f4282d121ec043404759a73286597
Newton Ni <nwtnni@gmail.com>
2025-10-01 14:01:06 -0500

    Output raw hdrhistogram
    Will need to update downstream plotting code to deserialize.

--- src/stat.rs
+++ src/stat.rs
@@ -10,15 +10,15 @@ static RECORD: AtomicBool = AtomicBool::new(false);
 
 #[cfg(feature = "stat")]
 thread_local! {
-    pub(crate) static THREAD: Thread = const { Thread::new() };
+    pub(crate) static THREAD: core::cell::RefCell<Thread> = core::cell::RefCell::new(Thread::default()) ;
 }
 
 pub fn process<K: crate::Key, V>(map: &mut crate::concurrent::Map<K, V>) -> Process {
-    let mut depth = Histogram::new();
-    let mut compression = Histogram::new();
-    let mut node_3 = Histogram::new();
-    let mut node_15 = Histogram::new();
-    let mut node_256 = Histogram::new();
+    let mut depth = Histogram::default();
+    let mut compression = Histogram::default();
+    let mut node_3 = Histogram::default();
+    let mut node_15 = Histogram::default();
+    let mut node_256 = Histogram::default();
 
     let mut entries = map
         .as_sequential()
@@ -59,11 +59,11 @@ pub fn process<K: crate::Key, V>(map: &mut crate::concurrent::Map<K, V>) -> Proc
     }
 
     Process {
-        depth: depth.into(),
-        compression: compression.into(),
-        node_3: node_3.into(),
-        node_15: node_15.into(),
-        node_256: node_256.into(),
+        depth,
+        compression,
+        node_3,
+        node_15,
+        node_256,
     }
 }
 
@@ -71,7 +71,7 @@ pub fn process<K: crate::Key, V>(map: &mut crate::concurrent::Map<K, V>) -> Proc
 pub fn thread() -> Thread {
     #[cfg(feature = "stat")]
     {
-        THREAD.with(|thread| thread.clone())
+        THREAD.with_borrow(|thread| thread.clone())
     }
 
     #[cfg(not(feature = "stat"))]
@@ -97,73 +97,18 @@ pub fn stop() {
 #[inline]
 pub fn reset() {
     #[cfg(feature = "stat")]
-    THREAD.with(|thread| thread.reset());
+    THREAD.with_borrow_mut(|thread| *thread = Thread::default());
 }
 
 #[derive(Default)]
 #[cfg_attr(feature = "stat", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(not(feature = "stat"), expect(unused))]
 pub struct Process {
-    depth: Distribution,
-    compression: Distribution,
-    node_3: Distribution,
-    node_15: Distribution,
-    node_256: Distribution,
-}
-
-#[derive(Default)]
-#[cfg_attr(feature = "stat", derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(not(feature = "stat"), expect(unused))]
-struct Distribution {
-    min: u64,
-    max: u64,
-    mean: f64,
-    p50: u64,
-    p75: u64,
-    p90: u64,
-    p99: u64,
-}
-
-struct Histogram {
-    #[cfg(feature = "stat")]
-    inner: hdrhistogram::Histogram<u64>,
-}
-
-impl Histogram {
-    fn new() -> Self {
-        Self {
-            #[cfg(feature = "stat")]
-            inner: hdrhistogram::Histogram::new(3).unwrap(),
-        }
-    }
-
-    fn record(&mut self, _value: u64) {
-        #[cfg(feature = "stat")]
-        self.inner.record(_value).unwrap();
-    }
-}
-
-impl From<Histogram> for Distribution {
-    fn from(_histogram: Histogram) -> Self {
-        #[cfg(not(feature = "stat"))]
-        {
-            Self::default()
-        }
-
-        #[cfg(feature = "stat")]
-        {
-            let histogram = _histogram.inner;
-            Self {
-                min: histogram.min(),
-                max: histogram.max(),
-                mean: histogram.mean(),
-                p50: histogram.value_at_quantile(0.5),
-                p75: histogram.value_at_quantile(0.75),
-                p90: histogram.value_at_quantile(0.9),
-                p99: histogram.value_at_quantile(0.99),
-            }
-        }
-    }
+    depth: Histogram,
+    compression: Histogram,
+    node_3: Histogram,
+    node_15: Histogram,
+    node_256: Histogram,
 }
 
 #[cfg_attr(not(feature = "stat"), expect(dead_code))]
@@ -205,97 +150,58 @@ impl From<node::Op> for Counter {
 }
 
 #[cfg(feature = "stat")]
-#[derive(Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
 pub struct Thread {
     node: Node,
     edge: Edge,
-    insert_pessimistic: core::cell::Cell<u64>,
-    retire: core::cell::Cell<u64>,
-    flush: core::cell::Cell<u64>,
-    retire_cache: core::cell::Cell<u64>,
-    free_conflict: core::cell::Cell<u64>,
-    free_retire: core::cell::Cell<u64>,
-    free_drop: core::cell::Cell<u64>,
-    hazard_match: core::cell::Cell<u64>,
+    insert_pessimistic: u64,
+    retire: u64,
+    flush: u64,
+    retire_cache: u64,
+    free_conflict: u64,
+    free_retire: u64,
+    free_drop: u64,
+    hazard_match: u64,
 }
 
 #[cfg(not(feature = "stat"))]
 pub struct Thread;
 
 #[cfg(feature = "stat")]
-#[derive(Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
 struct Node {
-    replace: core::cell::Cell<u64>,
-    shrink: core::cell::Cell<u64>,
-    grow: core::cell::Cell<u64>,
-    destroy: core::cell::Cell<u64>,
-    compress: core::cell::Cell<u64>,
+    replace: u64,
+    shrink: u64,
+    grow: u64,
+    destroy: u64,
+    compress: u64,
 }
 
 #[cfg(feature = "stat")]
-#[derive(Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
 struct Edge {
-    create: core::cell::Cell<u64>,
-    expand: core::cell::Cell<u64>,
-    insert: core::cell::Cell<u64>,
-    remove: core::cell::Cell<u64>,
+    create: u64,
+    expand: u64,
+    insert: u64,
+    remove: u64,
 }
 
 #[cfg(feature = "stat")]
 impl Thread {
-    const fn new() -> Self {
-        Self {
-            node: Node {
-                replace: core::cell::Cell::new(0),
-                shrink: core::cell::Cell::new(0),
-                grow: core::cell::Cell::new(0),
-                destroy: core::cell::Cell::new(0),
-                compress: core::cell::Cell::new(0),
-            },
-            edge: Edge {
-                create: core::cell::Cell::new(0),
-                expand: core::cell::Cell::new(0),
-                insert: core::cell::Cell::new(0),
-                remove: core::cell::Cell::new(0),
-            },
-            insert_pessimistic: core::cell::Cell::new(0),
-            retire: core::cell::Cell::new(0),
-            retire_cache: core::cell::Cell::new(0),
-            flush: core::cell::Cell::new(0),
-            free_conflict: core::cell::Cell::new(0),
-            free_retire: core::cell::Cell::new(0),
-            free_drop: core::cell::Cell::new(0),
-            hazard_match: core::cell::Cell::new(0),
-        }
-    }
-
-    fn reset(&self) {
-        self.node.replace.set(0);
-        self.node.shrink.set(0);
-        self.node.grow.set(0);
-        self.node.destroy.set(0);
-        self.node.compress.set(0);
-
-        self.edge.create.set(0);
-        self.edge.expand.set(0);
-        self.edge.insert.set(0);
-        self.edge.remove.set(0);
-    }
-
-    fn op(&self, op: raw::Op) -> &core::cell::Cell<u64> {
+    fn op(&mut self, op: raw::Op) -> &mut u64 {
         match op {
             raw::Op::Node(op) => match op {
-                crate::node::Op::Shrink => &self.node.shrink,
-                crate::node::Op::Replace => &self.node.replace,
-                crate::node::Op::Grow => &self.node.grow,
-                crate::node::Op::Destroy => &self.node.destroy,
-                crate::node::Op::Compress => &self.node.compress,
+                crate::node::Op::Shrink => &mut self.node.shrink,
+                crate::node::Op::Replace => &mut self.node.replace,
+                crate::node::Op::Grow => &mut self.node.grow,
+                crate::node::Op::Destroy => &mut self.node.destroy,
+                crate::node::Op::Compress => &mut self.node.compress,
             },
             raw::Op::Edge(op) => match op {
-                crate::edge::Op::Create => &self.edge.create,
-                crate::edge::Op::Expand => &self.edge.expand,
-                crate::edge::Op::Insert => &self.edge.insert,
-                crate::edge::Op::Remove => &self.edge.remove,
+                crate::edge::Op::Create => &mut self.edge.create,
+                crate::edge::Op::Expand => &mut self.edge.expand,
+                crate::edge::Op::Insert => &mut self.edge.insert,
+                crate::edge::Op::Remove => &mut self.edge.remove,
             },
         }
     }
@@ -305,18 +211,17 @@ impl Thread {
 pub(crate) fn increment<C: Into<Counter>>(_counter: C) {
     #[cfg(feature = "stat")]
     if RECORD.load(Ordering::Relaxed) {
-        THREAD.with(|thread| {
-            match _counter.into() {
+        THREAD.with_borrow_mut(|thread| {
+            *match _counter.into() {
                 Counter::Op(op) => thread.op(op),
-                Counter::InsertPessimistic => &thread.insert_pessimistic,
-                Counter::Retire => &thread.retire,
-                Counter::Flush => &thread.flush,
-                Counter::FreeConflict => &thread.free_conflict,
-                Counter::FreeRetire => &thread.free_retire,
-                Counter::FreeDrop => &thread.free_drop,
-                Counter::HazardMatch => &thread.hazard_match,
-            }
-            .update(|count| count + 1)
+                Counter::InsertPessimistic => &mut thread.insert_pessimistic,
+                Counter::Retire => &mut thread.retire,
+                Counter::Flush => &mut thread.flush,
+                Counter::FreeConflict => &mut thread.free_conflict,
+                Counter::FreeRetire => &mut thread.free_retire,
+                Counter::FreeDrop => &mut thread.free_drop,
+                Counter::HazardMatch => &mut thread.hazard_match,
+            } += 1;
         })
     }
 }
@@ -326,11 +231,69 @@ pub(crate) fn increment<C: Into<Counter>>(_counter: C) {
 pub(crate) fn max(_max: Max, _value: u64) {
     #[cfg(feature = "stat")]
     if RECORD.load(Ordering::Relaxed) {
-        THREAD.with(|thread| {
-            match _max {
-                Max::RetireCache => &thread.retire_cache,
-            }
-            .update(|count| count.max(_value))
+        THREAD.with_borrow_mut(|thread| {
+            let old = match _max {
+                Max::RetireCache => &mut thread.retire_cache,
+            };
+            *old = (*old).max(_value);
+        })
+    }
+}
+
+#[derive(Clone)]
+struct Histogram {
+    #[cfg(feature = "stat")]
+    inner: hdrhistogram::Histogram<u64>,
+}
+
+impl Histogram {
+    fn record(&mut self, _value: u64) {
+        #[cfg(feature = "stat")]
+        self.inner.record(_value).unwrap();
+    }
+}
+
+impl Default for Histogram {
+    fn default() -> Self {
+        Self {
+            #[cfg(feature = "stat")]
+            inner: hdrhistogram::Histogram::new(3).unwrap(),
+        }
+    }
+}
+
+#[cfg(feature = "stat")]
+impl serde::Serialize for Histogram {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use hdrhistogram::serialization::Serializer as _;
+        use hdrhistogram::serialization::V2DeflateSerializer;
+        use serde::ser::Error as _;
+
+        let mut buffer = Vec::new();
+        V2DeflateSerializer::new()
+            .serialize(&self.inner, &mut buffer)
+            .map_err(S::Error::custom)?;
+        serde_bytes::serialize(&buffer, serializer)
+    }
+}
+
+#[cfg(feature = "stat")]
+impl<'de> serde::Deserialize<'de> for Histogram {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use hdrhistogram::serialization::Deserializer;
+        use serde::de::Error as _;
+
+        let mut bytes: &[u8] = serde_bytes::deserialize(deserializer)?;
+        Ok(Histogram {
+            inner: Deserializer::new()
+                .deserialize(&mut bytes)
+                .map_err(D::Error::custom)?,
         })
     }
 }
