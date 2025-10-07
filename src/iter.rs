823fa049f8aee7132d5645255f2240e30638be33
Newton Ni <nwtnni@gmail.com>
2025-10-07 18:49:05 -0500

    Implement more iterator traits for `Or`

--- src/iter.rs
+++ src/iter.rs
@@ -49,4 +49,40 @@ where
             Or::R(right) => right.next(),
         }
     }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            Or::L(left) => left.size_hint(),
+            Or::R(right) => right.size_hint(),
+        }
+    }
+}
+
+impl<L, R, T> ExactSizeIterator for Or<L, R>
+where
+    L: ExactSizeIterator<Item = T>,
+    R: ExactSizeIterator<Item = T>,
+{
+    #[inline]
+    fn len(&self) -> usize {
+        match self {
+            Or::L(left) => left.len(),
+            Or::R(right) => right.len(),
+        }
+    }
+}
+
+impl<L, R, T> DoubleEndedIterator for Or<L, R>
+where
+    L: DoubleEndedIterator<Item = T>,
+    R: DoubleEndedIterator<Item = T>,
+{
+    #[inline]
+    fn next_back(&mut self) -> Option<Self::Item> {
+        match self {
+            Or::L(left) => left.next_back(),
+            Or::R(right) => right.next_back(),
+        }
+    }
 }
