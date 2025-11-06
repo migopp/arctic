1111ee69509b8190b0b52bc0c9a4b50e7b532157
Newton Ni <nwtnni@gmail.com>
2025-11-06 08:59:42 -0600

    Add doc comment explaining new raw module

--- src/raw.rs
+++ src/raw.rs
@@ -1,3 +1,15 @@
+//! This module contains the types defining the structure of the tree
+//! ([`crate::raw::edge`] and [`crate::raw::node`]) and (b) the core
+//! iteration (TODO) and traversal ([`crate::raw::cursor`]) logic.
+//! It is "raw" with respect to safe memory reclamation ([`crate::smr`])
+//! and value types ([`crate::value`]): users of this module must (a) ensure
+//! that memory is not reclaimed while iterating or traversing the tree,
+//! and (b) provide meaning to the raw u64 values.
+//!
+//! This separation has two benefits:
+//! - Reduced compilation time and code duplication from monomorphization (esp. of value types).
+//! - Reuse of iteration and traversal code between the concurrent and sequential maps.
+
 pub(crate) mod cursor;
 pub(crate) mod edge;
 pub(crate) mod node;
