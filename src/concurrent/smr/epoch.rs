76a35d1bdb20144915ba0122b508c30befbe58ff
Newton Ni <nwtnni@gmail.com>
2026-04-03 16:37:58 -0500

    Embed local state in global

--- src/concurrent/smr/epoch.rs
+++ src/concurrent/smr/epoch.rs
@@ -1,37 +1,66 @@
-use crate::concurrent::smr;
+use core::cell::UnsafeCell;
+
+use crossbeam_epoch::LocalHandle;
+
 use crate::concurrent::Smr;
 use crate::concurrent::Value;
+use crate::concurrent::smr;
 use crate::raw::node;
 use crate::stat;
 
-#[derive(Default)]
-pub struct Epoch(crossbeam_epoch::Collector);
+pub struct Epoch {
+    collector: crossbeam_epoch::Collector,
+    locals: [UnsafeCell<Option<LocalHandle>>; smr::thread::MAX],
+}
+
+impl Default for Epoch {
+    fn default() -> Self {
+        Self {
+            collector: crossbeam_epoch::Collector::default(),
+            locals: core::array::from_fn(|_| UnsafeCell::new(None)),
+        }
+    }
+}
 
 impl Epoch {
     pub fn with_bag_capacity(max_objects: usize) -> Self {
         crossbeam_epoch::set_bag_capacity(max_objects);
         Self::default()
     }
-}
 
-impl<P: ribbit::Pack<Packed: smr::hazard::Prefix>, V: Value> Smr<P, V> for Epoch {
-    type Local<'g> = Local;
+    fn local(&self) -> &LocalHandle {
+        let id = smr::thread::Id::current();
+        let local = &self.locals[usize::from(id)];
+        match unsafe { local.get().as_ref().unwrap() } {
+            Some(local) => local,
+            None => self.local_cold(),
+        }
+    }
 
-    fn local<'g>(&'g self) -> Self::Local<'g> {
-        Local(self.0.register())
+    #[cold]
+    fn local_cold(&self) -> &LocalHandle {
+        let id = smr::thread::Id::current();
+        let local = &self.locals[usize::from(id)];
+        unsafe { local.get().as_mut().unwrap() }.insert(self.collector.register())
     }
 }
 
-pub struct Local(crossbeam_epoch::LocalHandle);
+impl<P: ribbit::Pack<Packed: smr::hazard::Prefix>, V: Value> Smr<P, V> for Epoch {
+    type Local<'g> = &'g LocalHandle;
 
-impl<P: ribbit::Pack<Packed: smr::hazard::Prefix>, V: Value> smr::Local<P, V> for Local {
+    fn local<'g>(&'g self) -> Self::Local<'g> {
+        self.local()
+    }
+}
+
+impl<P: ribbit::Pack<Packed: smr::hazard::Prefix>, V: Value> smr::Local<P, V> for &'_ LocalHandle {
     type Guard<'l>
         = Guard
     where
         Self: 'l;
 
     fn guard<'l>(&'l mut self, _hazard: ribbit::Packed<P>) -> Self::Guard<'l> {
-        Guard(self.0.pin())
+        Guard(self.pin())
     }
 }
 
@@ -47,14 +76,18 @@ impl<V: Value> smr::Guard<V> for Guard {
     ) {
         stat::increment(stat::Counter::Retire);
 
-        self.0.defer_unchecked(move || {
-            node.deallocate(stat::Counter::FreeRetire);
-        });
+        unsafe {
+            self.0.defer_unchecked(move || {
+                node.deallocate(stat::Counter::FreeRetire);
+            });
+        }
     }
 
     unsafe fn retire_value(&mut self, value: u64) {
         stat::increment(stat::Counter::Retire);
 
-        self.0.defer_unchecked(move || drop(V::from_raw(value)));
+        unsafe {
+            self.0.defer_unchecked(move || drop(V::from_raw(value)));
+        }
     }
 }
